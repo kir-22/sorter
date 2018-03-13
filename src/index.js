0cfcbc29bcf5a871ec852edff8f7f96afb48bee1
kir-22 <kirill89kireew@gmail.com>
2018-03-13 13:37:54 +0300

    Ok

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.array=new Array();
+    this.compareFunc=function(a,b){return a-b;}
   }
 
   add(element) {
     // your implementation
+    this.array.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.array[index];
   }
 
   get length() {
     // your implementation
+    return this.array.length;
   }
 
   toArray() {
     // your implementation
+    return this.array;
   }
 
   sort(indices) {
     // your implementation
+    indices.sort(function(a,b){return a-b;})
+    var arr=[];
+    for(var i=0; i<indices.length; i++){arr.push(this.array[indices[i]]);}
+    arr.sort(this.compareFunc);
+    for(var j=0;j<indices.length;j++){this.array.splice(indices[j],1 arr[j]);}
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compareFunc=compareFunction;
   }
 }
 
